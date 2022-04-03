f1bc9aaf6108006c2fbe7f103ec5da1b1dda47e9
Gülşah Alıcı <gulsah.alici1@gmail.com>
2022-04-03 23:00:05 +0300

    install naive-ui and create sign in sign up forms

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <n-space justify="center">
+    <n-card title="Vue Firabase Authentication" style="margin-bottom: 16px; width: 500px">
+      <n-tabs default-value="signin" size="large" justify-content="space-evenly">
+        <n-tab-pane name="signin" tab="Sign in">
+          <n-form>
+            <n-form-item-row label="E-mail">
+              <n-input />
+            </n-form-item-row>
+            <n-form-item-row label="Password">
+              <n-input type="password"/>
+            </n-form-item-row>
+          </n-form>
+          <n-button type="primary" block secondary strong>
+            Sign In
+          </n-button>
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="Sign up">
+          <n-form>
+            <n-form-item-row label="E-mail">
+              <n-input />
+            </n-form-item-row>
+            <n-form-item-row label="Password">
+              <n-input type="password"/>
+            </n-form-item-row>
+          </n-form>
+          <n-button type="primary" block secondary strong>
+            Sign up
+          </n-button>
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>
+  </n-space>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton,NCard, NSpace } from 'naive-ui'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton, NCard, NSpace
   }
 }
 </script>
